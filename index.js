8004120c86729b54d3bcb1ff50a6d4b31d4dad37
Jorge Mollericon <jlmollericon@gmail.com>
2021-07-26 17:59:19 -0400

    add index.js, test

---
+++ index.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+/* esto es una prueba */
+
+function mono () {
+  console.log('monooo')
+}
+
+mono()
+
+console.log('HOLA Y PROBANDO')
+const x = 'hello'
+console.log('Hello' + x)
